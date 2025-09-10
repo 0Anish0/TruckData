651072d3159217259dd157e24b7dd2dd3d117dd6
Anish Pandey <anishpandey4576@gmail.com>
2025-09-10 16:07:39 +0530

    Remove unused import of NavigationProp in navigation types file to enhance code clarity and maintainability.

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,4 +1,4 @@
-import { NavigationProp, CompositeNavigationProp } from '@react-navigation/native';
+import { CompositeNavigationProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { Truck, TripWithRelations } from './index';
